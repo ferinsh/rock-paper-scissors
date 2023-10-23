b372a9631ab7bd4c12af0a1891564f678404ae00
Ferin Sharaf <146843308+ferinsh@users.noreply.github.com>
2023-10-23 17:40:37 +0530

    Beta version 3.0.0 --Major changes to code.

--- game/game.js
+++ game/game.js
@@ -1,126 +1,60 @@
-//FUNCTIONS
-//function definitions
-function playRound(roundChoice, roundScore, tempChoicePlayer){    //roundChoice == choice, roundScore == score
-    
-    roundChoice[0] = tempChoicePlayer;                     //player choice  
-    roundChoice[1] = getComputerChoice();        //computer choice
-    
-    
-    if(roundChoice[0] == "rock" && roundChoice[1] == "rock"){
-        roundScore[1] += 1;
-        console.log("draw");
-    }
-    if(roundChoice[0] == "paper" && roundChoice[1] == "paper"){
-        roundScore[1] += 1;
-        console.log("draw");
-    }
-    if(roundChoice[0] == "scissor" && roundChoice[1] == "scissor"){
-        roundScore[1] += 1;
-        console.log("draw");
-    }
-    if(roundChoice[0] == "rock" && roundChoice[1] == "scissor"){
-        roundScore[0] += 1;
-        console.log("player wins");
-    }
-    if(roundChoice[0] == "paper" && roundChoice[1] == "rock"){
-        roundScore[0] += 1;
-        console.log("player wins");
-    }
-    if(roundChoice[0] == "scissor" && roundChoice[1] == "paper"){
-        roundScore[0] += 1;
-        console.log("player wins");
-    }
-    if(roundChoice[0] == "rock" && roundChoice[1] == "paper"){
-        roundScore[2] += 1;
-        console.log("computer wins");
-    }
-    if(roundChoice[0] == "paper" && roundChoice[1] == "scissor"){
-        roundScore[2] += 1;
-        console.log("computer wins");
-    }
-    if(roundChoice[0] == "scissor" && roundChoice[1] == "rock"){
-        roundScore[2] += 1;
-        console.log("computer wins");
-    }
-    
-    if(round[0]<round[1]){
-        console.log("Over");
-        console.log('OVER');
-        console.log(score);
-        console.log("score reset");
-        console.log(score);
-    }
+var playerScore = 0
+var computerScore = 0
+const buttons = document.querySelectorAll('input')
 
-    round[0]++;
-    console.log(roundScore);
-
+function computerPlay(){
+    var choices = ['rock', 'paper', 'scissor']
+    return choices[Math.floor(Math.random() * choices.length)]
 }
 
-function getComputerChoice(){
-    const TEMP = ["rock", "paper", "scissor"];
-    const TEMP_RANDOM = Math.floor(Math.random() * 3);
-    return TEMP[TEMP_RANDOM];
+function disableButtons(){
+    buttons.forEach((button) => {
+        button.disabled = true
+    });
 }
 
-function getWinner(){
-    if(score[0] == score[2]){
-        return "game is a draw"
-    }
-    if(score[0] > score[2]){
-        return "player wins the game";
-    }
-    if(score[0] < score[2]){
-        return "computer wins the game";
-    }
-}
+function playRound(playerSelection){
+    var computerSelection = computerPlay()
+    var result = ""
 
-function btnCallRock(){
-    btnRock.addEventListener('click', console.log("rock"));
-}
+    if((playerSelection == "rock" && computerSelection == "scissor") || 
+       (playerSelection == "scissor" && computerSelection == "paper") ||
+       (playerSelection == "paper" && computerSelection == "rock")){
 
-
-function playGame(roundNum){
+        playerScore += 1
+        result = ('You win!' + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
+        
+        if (playerScore == 5){
+            result += '<br><br>You won the game! Reload the page to play again'
+            disableButtons() 
+        }
+    }
+    else if(playerSelection == computerSelection) {
+        result = ("It's a tie. You both choose " + playerSelection
+                + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
+    }
+    else{
+        computerScore += 1
+        result = ('You lose! ' + computerSelection + ' beats ' + playerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
     
+        if (computerScore == 5) {
+            result += '<br><br>I won the game! Reload the page to play again'
+            disableButtons()
+        }
+    }
+
+    document.getElementById('result').innerHTML = result
+    document.getElementById('player-selection').textContent = playerSelection
+    return
+
 }
 
 
 
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playRound(button.value)
 
-//GAME MEMORY
-//Defintions
-
-//variables
-
-
-
-var round = [0,4];            //[Current round number, Total round number]
-var score = [0, 0, 0];        //[player, draw, computer]
-var choice = ["", ""];        //[player, computer]
-var winner = "DRAW";
-
-
-//constants
-
-const btnRock = document.querySelector('#rock');
-const btnPaper = document.querySelector('#paper');
-const btnScissor = document.querySelector('#scissor');
-
-
-
-
-
-//GAME LOGIC
-
-
-
-
-//function calls
-
-
-
-playGame(round);
-
-
-
-
-//memory value tests
+    })
+})
